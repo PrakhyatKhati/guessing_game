2f13e495862851673f47efb204a7ba76cf1ad8b1
PrakhyatKhati <khatiprakhyat1@gmail.com>
2025-09-04 12:31:04 -0600

    Finished Guessing game.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
 use std::io; 
+use std::cmp::Ordering;
+
+use rand::Rng;
+// the above step is to bring the type into scope from the standard library.
+
 
 fn main() {
-    println!("Guess the number");
-    println!("Please input your guess");
-
-    let mut guess = String::new(); // making the variable mutuable
-    // as new value will be stored in this variable.
+    // println!("Guess the number");
+    let secret_numeber = rand::thread_rng().gen_range(1..=100);
+    //println!("The secret number is :{secret_numeber}");
     
+
+   loop{ println!("Please input your guess");
+        let mut guess = String::new(); // making the variable mutuable
+    // as new value will be stored in this variable.
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+        let guess: u32= match guess.trim().parse(){Ok(num)=>num,Err(_)=>continue,};
+        //println!("Please input your guess");
+        println!("You guessed : {guess}");
+
+
+        // match is an expression to decide what to do next based on what was returend.
+        match guess.cmp(&secret_numeber){
+            Ordering::Less => println!("Too small !!"),
+            Ordering::Greater =>println!("Too big"),
+            Ordering::Equal => {println!("You win !!");
+                                break;}
+            // here we are adding the break to break after right guess.
+    }
+}
 
 
 
