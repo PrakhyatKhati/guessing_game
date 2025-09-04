755c3ab59e072c588149aa0c436aead4d5abf922
PrakhyatKhati <khatiprakhyat1@gmail.com>
2025-09-04 11:18:27 -0600

    Day_1rust

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use std::io; 
+
+fn main() {
+    println!("Guess the number");
+    println!("Please input your guess");
+
+    let mut guess = String::new(); // making the variable mutuable
+    // as new value will be stored in this variable.
+    
+
+
+
+}
